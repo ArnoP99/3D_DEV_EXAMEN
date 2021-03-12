1608763015711fd1093dd23c34e7d2f26cf09a34
Arno Peeters <arno.peeters@student.kdg.be>
2021-03-13 00:17:14 +0100

    Added Basic UI + Gameplay Components
    - Added a crosshair with raytracing
    - Added Collectibles (Exams) that can be selected
    - Added a HUD with usefull information
    - Added colliders at main exits to complete the game
    
    (Creds aan Gilles want die heeft alles uitgezocht/gefixt vanmiddag, ik heb het enkel in ons definitief project geimplementeerd)

---
+++ 3D_DEV_GAME/Assets/Scripts/selectionmanager.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class selectionmanager : MonoBehaviour
+{
+    [SerializeField] private Material highlightMaterial;
+    [SerializeField] private Material defaultMaterial;
+
+    public Text score;
+    private Transform _selection;
+    private int scoreStart = 0;
+    public float distance;
+    private string standardText = "/9 Exams Collected";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        score.text = scoreStart.ToString() + standardText;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_selection != null)
+        {
+            var selectionRenderer = _selection.GetComponent<Renderer>();
+            selectionRenderer.material = defaultMaterial;
+            _selection = null;
+        }
+
+
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit) && hit.transform.tag == "Examen" && hit.distance < distance)
+        {
+            Debug.Log("hit");
+            var selection = hit.transform;
+            var selectionRenderer = selection.GetComponent<Renderer>();
+            if (selectionRenderer != null)
+            {
+                selectionRenderer.material = highlightMaterial;
+                if (Input.GetMouseButtonDown(0))
+                {
+                    Destroy(hit.transform.gameObject);
+                    scoreStart++;
+                    score.text = scoreStart.ToString() + standardText;
+                }
+            }
+            _selection = selection;
+        }
+    }
+}
